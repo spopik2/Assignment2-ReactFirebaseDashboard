5a6fe7b262cd88b41afa7c555347e21d53ff92c9
spopik2 <70964549+spopik2@users.noreply.github.com>
2022-04-08 20:57:07 -0600

    finised the AllProductsPanel

--- src/components/products/productcaard/ProductCard.js
+++ src/components/products/productcaard/ProductCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Button} from 'ui/buttons';
+import { useNavigate } from 'react-router-dom';
 
+import { Button} from 'ui/buttons';
 import {ProductCardStyles, Card, CardDiv} from './styles'
 
 
-
-
 function ProductCard ({children, product, ...props})  {
+  const navigator = useNavigate()
   const  {productName, productPrice, imageUrl, productDescription} = {...product}
   return (
         <ProductCardStyles  {...props}>
@@ -17,7 +17,7 @@ function ProductCard ({children, product, ...props})  {
               <p>{productDescription}</p>
               <h3>{productPrice}</h3>
               <div>
-                <Button width="5 rem" height='2rem' margin="1rem 1.5rem 0 0" bc='#19CBFF' color='white'>Edit</Button>
+                <Button width="5 rem" height='2rem' margin="1rem 1.5rem 0 0" bc='#19CBFF' color='white' onClick={()=> navigator('edit')}>Edit</Button>
                 <Button width="5 rem" bc='#F44336' color='white'>Delete</Button>
               </div>
             </CardDiv>
